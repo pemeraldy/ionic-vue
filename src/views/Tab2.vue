074ea3ff7643cf957f093f39eaff6a1c56d0786b
pemeraldy <pemeraldy@gmail.com>
2021-01-25 08:31:15 +0100

    initialsetup

---
+++ src/views/Tab2.vue
@@ -0,0 +1,50 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Yopee Gallery</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+        <ion-fab-button @click="takePhoto()">
+          <ion-icon :icon="camera"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { camera, trash, close } from "ionicons/icons";
+import { usePhotoGallery } from "@/composables/usePhotoGallery";
+import {
+  IonPage,
+  IonHeader,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from "@ionic/vue";
+
+export default {
+  name: "Tab2",
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+  },
+  setup() {
+    const { takePhoto } = usePhotoGallery();
+    return { takePhoto, camera, trash, close };
+  },
+};
+</script>
